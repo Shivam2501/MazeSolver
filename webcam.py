d7b5cebc43b8b68b23aee9a10d2969a936432de0
Srujun Gupta <theununtrium@gmail.com>
2015-11-15 12:46:00 -0600

    Added Canny filter

--- webcam.py
+++ webcam.py
@@ -10,15 +10,16 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 100)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    #cv2.imshow('frame', frame)
+    #cv2.imshow('frame',gray)
+    cv2.imshow('frame', edges)
     #if cv2.waitKey(1) & 0xFF == ord('q'):
     #    break
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite('framesave.png', gray)
+        cv2.imwrite('framesave.png', edges)
         break
 
 # When everything done, release the capture
